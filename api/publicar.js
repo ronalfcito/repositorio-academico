15cb9ee7b90208945d080972227f83145de08e4e
ronalfcito <tu-correo@ejemplo.com>
2026-06-18 16:43:53 -0500

    Debug fecha

--- api/publicar.js
+++ api/publicar.js
@@ -21,6 +21,8 @@ export default async function handler(req, res) {
   const sql = neon(process.env.DATABASE_URL);
 
   try {
+    console.log("FECHA RECIBIDA:", fecha);
+    console.log("TIPO:", typeof fecha);
 
     // Convertir fecha a formato YYYY-MM-DD
     let fechaSQL = fecha;
